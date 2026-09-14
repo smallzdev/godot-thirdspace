a9054cf2bf85d32e27e274a26c7ed3ec33fa694b
smallz <233221808+smallzdev@users.noreply.github.com>
2026-09-14 19:43:01 +1000

    Big Update
    Reorganised files, updated scene appearance, script, inputs and more! I also added a main menu scene and node to use later. For now, it's just yellow.

--- character.gd
+++ character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -1000.0
 @onready var mainSprite = %MainCharacter
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +11,21 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		mainSprite.animation = "jumping"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		mainSprite.animation = "running"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		mainSprite.animation = "default"
 
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	mainSprite.flip_h = isLeft
