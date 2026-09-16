df5adc50fe82b6aa5100c9d9980db63f7db3217b
smallz <233221808+smallzdev@users.noreply.github.com>
2026-09-16 20:15:47 +1000

    Game over lives code update
    Made a game over menu (although buttons dont work yet), I spent a while figuring out how to do code and fixing my code for falling, lives, and more! Not too much though, as I struggled a heap and I'm a begginer.

--- character.gd
+++ character.gd
@@ -3,13 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -950.0
+var lives = 3
+var gameoverscene = preload("res://game_over.tscn")
+
 @onready var mainSprite = %MainCharacter
 
-# 	var fall := Input.get_axis("Down")
-	# if direction:
-		# velocity.y = direction * SPEED
-			# print("test")
-
+# Fetches the current startposition :D
+func _ready() -> void:
+	var startposition = global_position
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -20,20 +22,25 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		mainSprite.animation = "jumping"
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
+	# uses input map ^
 	if direction:
 		velocity.x = direction * SPEED
 		mainSprite.animation = "running"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		mainSprite.animation = "default"
-		
 	if not is_on_floor() and global_position.y > 1000:
-		get_tree().reload_current_scene()
-
+		var startposition: Vector2
+		if lives == 0:
+			print("All lives used, game over D:")
+			get_tree().change_scene_to_packed(gameoverscene)
+		lives -= 1
+		print(lives)
+		global_position = startposition
+		
 	move_and_slide()
 	
 	var isLeft = velocity.x < 0
 	mainSprite.flip_h = isLeft
+	
