238ca66a85033f064f554bdbfe919240d95c2a02
smallz <233221808+smallzdev@users.noreply.github.com>
2026-09-24 22:22:59 +1000

    Chest Animation + GUI Spacing
    1. Activated the chest animation. Now when you hold for 3s, it opens the chest and plays an animation. This took me a bit to understand. There's also a label, which displays how long left you need to hold E for.
    
    2. Spaced the GUI for hearts and coins on level 2 out a bit, because they got squashed when going into the double digits.
    
    3. I might've done more, but I forgot to write it.

--- scripts/chest.gd
+++ scripts/chest.gd
@@ -4,10 +4,11 @@ extends Area2D
 var holdSec = 0
 var chestInteract = false
 @onready var chestSprite = %chestSprite2D
+@onready var chestTimerLabel = %chestTimerLabel
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
+func _ready() -> void:
+	chestSprite.frame = 0
 # on collision, it sets chest interaction to true
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
@@ -18,14 +19,21 @@ func _on_body_entered(body: Node2D) -> void:
 func _process(delta) -> void:
 	if Input.is_action_pressed("Interact"):
 		holdSec += delta
+		# Countdown timer, rounds so its not crazy
+		chestTimerLabel.text = str(round(3 - holdSec))
 		
+		# the following if statment checks if the character is
+		# is in range, the user held E for 3s, and it isn't
+		# already open by checking the animation frame
 		if holdSec >= 3 and chestInteract == true and chestSprite.frame == 0:
 			# uses print for debugging
 			print("Held E for 3 secs, chest interacted.")
 			holdSec = 0
-			chestSprite.frame = 2
+			chestSprite.play("opened")
 			Global.add_coins(3)
+			chestTimerLabel.visible = false
 			
 		elif holdSec >=3 and chestSprite.frame <= 1:
 			print("already interacted with chest")
+			holdSec = 0
 			# planning something here shortly, for now just print
